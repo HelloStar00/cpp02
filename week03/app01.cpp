808daa3264210e40eae98df3359a1667e436b6d9
202330235 <202330235@email.daelim.ac.kr>
2025-09-11 11:28:44 +0900

    v0.2 week03 add virtual

--- week03/app01.cpp
+++ week03/app01.cpp
@@ -4,25 +4,36 @@ using namespace std;
 
 class Pokemon {
 public:
-	void attack() const { cout << "attack" << endl; }
+	virtual void attack() const { cout << "attack" << endl; }
 };
 
 class Picachu : public Pokemon	// is-a
 {
 public:
-	void attack() const { cout << "electric attack" << endl; }
+	virtual void attack() const { cout << "electric attack" << endl; }
 };
 
 int main() {
-	// 포켓몬 클래스에 대한 포인터 (소켓) 생성
+
 	Pokemon* pokemon;
-	// 포인터로 포켓몬 클래스의 객체 가리키기
+
 	pokemon = new Pokemon();
 	pokemon->attack();
 	delete pokemon;
-	// 포인터로 피카츄 클래스의 객체 가리키기
+
 	pokemon = new Picachu();
 	pokemon->attack();
 	delete pokemon;
 	return 0;
 }
+
+/*
+	출력 :
+	attack
+	attack
+*/
+
+/*
+	virtual 추가 후 출력:
+
+*/
